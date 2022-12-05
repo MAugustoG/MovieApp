992b784e42bb2c227af928c0154daff0d8b99fc6
Mateus Augusto Gomes <mataugustogomes@gmail.com>
2022-12-05 02:13:59 +0000

    Created Search page

--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -1,3 +1,66 @@
+// -----------------------------------------------------------------------------
+// 'EXTERNAL' IMPORTS
+// -----------------------------------------------------------------------------
+import { useEffect, useMemo } from 'react';
+import { useNavigate, useSearchParams } from 'react-router-dom';
+
+// -----------------------------------------------------------------------------
+// 'INTERNAL' IMPORTS
+// -----------------------------------------------------------------------------
+import { EmptySection } from '../../shared/components/EmptySection';
+import { ErrorSection } from '../../shared/components/ErrorSection';
+import { Movie } from '../../shared/types/movies/common/movie.types';
+import { useSearchMovie } from '../../services/hooks/useSearchMovie';
+import { InfiniteMovieList } from '../../shared/components/InfiniteMovieList';
+
 export const Search = () => {
-  return <h1>Search</h1>;
+  const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
+  const query = searchParams.get('q');
+
+  useEffect(() => {
+    if (!query) {
+      navigate(`/`);
+    }
+  }, [query]);
+
+  const {
+    data,
+    error,
+    isFetching,
+    hasNextPage,
+    fetchNextPage,
+    isFetchingNextPage,
+  } = useSearchMovie({ title: `${query}` });
+
+  const handleLoadNextPage = () => {
+    fetchNextPage();
+  };
+
+  const movies: Movie[] = useMemo(() => {
+    if (!data) return [];
+
+    let auxMovies: Movie[] = [];
+
+    data.pages.forEach(
+      ({ results }) => (auxMovies = [...auxMovies, ...results])
+    );
+
+    return auxMovies;
+  }, [data]);
+
+  if (error) return <ErrorSection />;
+
+  if (!movies.length && !isFetching)
+    return <EmptySection label={`Nothing searched for: ${query}`} />;
+
+  return (
+    <InfiniteMovieList
+      data={movies}
+      isFetching={isFetching}
+      hasNextPage={hasNextPage}
+      fetchNextPage={handleLoadNextPage}
+      isFetchingNextPage={isFetchingNextPage}
+    />
+  );
 };
